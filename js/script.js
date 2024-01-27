052f4448d8865f550e664b1d8d024b0632d54903
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-27 20:45:47 +0000

    Added add and delete Task

---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log('Vue ok', Vue)
+
+const { createApp } = Vue;
+
+const app = createApp({
+    name: 'VueToDo',
+    data: () => ({
+        tasks: [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ],
+        newTask: ''
+
+    }),
+    // Cancello i Task
+    methods: {
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => id !== task.id)
+        },
+        // Aggiungo un nuovo Task
+        addTask() {
+            const newTask = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTask
+            }
+            this.tasks.push(newTask)
+            // Svuoto la stringa dopo averla inviata
+            this.newTask = ''
+        }
+    }
+})
+
+app.mount('#root');
